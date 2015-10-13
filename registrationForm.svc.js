1926b81fece272ee0f6fc4bb1bf5f75ae1573330
John Hall <jhall@logictree.com>
2015-10-13 17:01:07 -0600

    formatting

--- registrationForm.svc.js
+++ registrationForm.svc.js
@@ -1,39 +1,40 @@
 registrationFormMod.
 
-factory( 'registrationSvc', function() {
-   var profile = {
-           userType: null,
-           firstName: 'Daniel',
-           lastName: 'Hogan',
-           phone: '3032752331',
-           email1: 'dan_hogan@fws.gov',
-           email2: 'dan_hogan@fws.gov',
-           regionId: null,
-           stateId: null,
-           organization: 'U.S. Fish and Wildlife Service',
-           dataentry: false,
-           fedreview: true,
-           useradmin: true,
-           fbmsReports: false,
-           password1: null,
-           password2: null
-       }
+    factory('registrationSvc', function () {
+        var profile = {
+            userType: null,
+            firstName: 'Daniel',
+            lastName: 'Hogan',
+            phone: '3032752331',
+            email1: 'dan_hogan@fws.gov',
+            email2: 'dan_hogan@fws.gov',
+            regionId: null,
+            stateId: null,
+            organization: 'U.S. Fish and Wildlife Service',
+            dataentry: false,
+            fedreview: true,
+            useradmin: true,
+            fbmsReports: false,
+            password1: null,
+            password2: null
+        };
 
-   return {
-      save: function(profile){
-      },
-      findById: function(id){
-        return profile;
-      }
-    };
-});
+        return {
+            save: function (profile) {
+            },
+            findById: function (id) {
+                return profile;
+            }
+        }
+    });
 
 registrationFormMod.
-factory('registrationSvc', function ($rootScope, $timeout) {
-    return {
-        focusIt: function(name) {
-        $timeout(function () {
-            $rootScope.$broadcast('focusMe', name);
-        });
-    }}
-});
+    factory('registrationSvc', function ($rootScope, $timeout) {
+        return {
+            focusIt: function (name) {
+                $timeout(function () {
+                    $rootScope.$broadcast('focusMe', name);
+                });
+            }
+        }
+    });
