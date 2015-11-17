b7963d7481e560b67ea214c072a0550bc8fa304a
John Hall <jhall@logictree.com>
2015-11-17 13:32:50 -0700

    Added karma-ng-html2js-preprocessor and configured it.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -11,7 +11,10 @@ module.exports = function (config) {
             'bower_components/jquery/dist/jquery.js',
             'dist/uploader.js',
             '*.js',
-            'test/unit/*.js'
+            'test/unit/*.js',
+            '*.html',
+            // if you wanna load template files in nested directories, you must use this
+            '**/*.html'
         ],
 
         autoWatch: true,
@@ -23,9 +26,48 @@ module.exports = function (config) {
 
         plugins: [
             'karma-chrome-launcher',
+            'karma-ng-html2js-preprocessor',
             'karma-jasmine',
             'karma-qunit'
         ],
+
+        preprocessors: {
+            '**/*.html': ['ng-html2js']
+        },
+
+        // if you have defined plugins explicitly, add karma-ng-html2js-preprocessor
+        // plugins: [
+        //     <your plugins>
+        //     'karma-ng-html2js-preprocessor',
+        // ]
+
+        ngHtml2JsPreprocessor: {
+            // strip this from the file path
+            stripPrefix: 'test/',
+            //stripSuffix: '.ext',
+            // prepend this to the
+            //prependPrefix: 'templates/',
+
+            // or define a custom transform function
+            // - cacheId returned is used to load template
+            //   module(cacheId) will return template at filepath
+/*
+            cacheIdFromPath: function (filepath) {
+                // example strips 'public/' from anywhere in the path
+                // module(app/templates/template.html) => app/public/templates/template.html
+                var cacheId = filepath.strip('test/', '');
+                return cacheId;
+            },
+*/
+
+            // - setting this option will create only a single module that contains templates
+            //   from all the files, so you can load them all with module('foo')
+            // - you may provide a function(htmlPath, originalPath) instead of a string
+            //   if you'd like to generate modules dynamically
+            //   htmlPath is a originalPath stripped and/or prepended
+            //   with all provided suffixes and prefixes
+            moduleName: 'uploadModule'
+        },
 
         junitReporter: {
             outputFile: 'test_out/unit.xml',
