66a4a1459d78c2d602523a9b2c2371994a9963c0
John Hall <ojhsolutions@gmail.com>
2015-11-23 18:19:03 -0700

    formatting

--- gov.doi.tracs.forms.js
+++ gov.doi.tracs.forms.js
@@ -1,36 +1,36 @@
 var tracsFormsMod = angular.module('gov.doi.tracs.forms', ['ui.bootstrap']);
 
 tracsFormsMod.
-directive( 'txFormField', function() {
+directive('txFormField', function () {
     return {
         restrict: 'E',
         scope: {
-          tfType: '@',
-          tfPlaceholder: '@',
-          tfLabel: '@',
-          tfId: '@',
-          tfHelp: '@',
-          tfModel: '=',
-          tfForm: '=',
-          tfRequired: '='
+            tfType: '@',
+            tfPlaceholder: '@',
+            tfLabel: '@',
+            tfId: '@',
+            tfHelp: '@',
+            tfModel: '=',
+            tfForm: '=',
+            tfRequired: '='
         },
         templateUrl: './templates/formField.template.html',
         transclude: true,
-        controller: ['$scope', function($scope) {
+        controller: ['$scope', function ($scope) {
 
-              this.getId = function() {
+            this.getId = function () {
                 return $scope.tfId;
-              };
+            };
 
-              this.getType = function() {
+            this.getType = function () {
                 return $scope.tfType;
-              }
+            }
         }]
     }
 });
 
 tracsFormsMod.
-directive( 'txTextInput', function() {
+directive('txTextInput', function () {
     return {
         restrict: 'E',
         scope: {
@@ -40,19 +40,19 @@ directive( 'txTextInput', function() {
         },
         require: '^txFormField',
         templateUrl: './templates/textInput.template.html',
-        link: function(scope, element, attrs, ctrl) {
+        link: function (scope, element, attrs, ctrl) {
 
-              scope.tfId = ctrl.getId();
-              scope.tfType = ctrl.getType();
+            scope.tfId = ctrl.getId();
+            scope.tfType = ctrl.getType();
         }
     }
 });
 
 tracsFormsMod.
-directive('focusMe', function() {
-    return function(scope, element, attribute) {
-        scope.$on('focusMe', function(e, name) {
-            if(name === attribute.focusMe) {
+directive('focusMe', function () {
+    return function (scope, element, attribute) {
+        scope.$on('focusMe', function (e, name) {
+            if (name === attribute.focusMe) {
                 element[0].focus();
             }
         });
