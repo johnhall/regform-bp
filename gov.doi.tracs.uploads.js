19cca53a3afd4495575b1a8381145368e0fdc545
John Hall <ojhsolutions@gmail.com>
2015-11-24 16:52:11 -0700

    Bound the metadata to the files being uploaded and fixed the naming conventions

--- gov.doi.tracs.uploads.js
+++ gov.doi.tracs.uploads.js
@@ -1,7 +1,7 @@
 var tracsUploadsMod = angular.module('gov.doi.tracs.uploads', ['ui.bootstrap', 'gov.doi.tracs.forms', 'uploadModule']);
 
 tracsUploadsMod.
-directive('fileUploadWithMetadata', function () {
+directive('txFileUploadMetadata', function () {
     return {
         restrict: 'E',
         scope: {
@@ -12,11 +12,32 @@ directive('fileUploadWithMetadata', function () {
         },
         templateUrl: './templates/fileFormWithMetadata.html',
         controllerAs: 'fUpMetaMain',
-        controller: function ($scope, $element, $attrs) {
+        controller: function ($scope, $element, $attrs, $transclude) {
             this.metadata = {};
         },
         link: function(scope, element, attrs, ctrl) {
-            //console.log(scope, element, attrs, ctrl);
+            //console.log(scope);
+            //console.log(element);
+            //console.log(attrs);
+            //console.log(ctrl);
+            scope.$on('fileuploadsubmit', function (e, data) {
+/*
+                var title = $('#' + scope.tfId + '-title');
+                data.formData = {title: title.val()};
+*/
+                data.formData = {title: ctrl.metadata.title,
+                                author: ctrl.metadata.author,
+                                caption: ctrl.metadata.caption,
+                                fileType: ctrl.metadata.fileType};
+                console.log(data.formData);
+/*
+                if (!data.formData.title) {
+                    data.context.find('button').prop('disabled', false);
+                    title.focus();
+                    return false;
+                }
+*/
+            });
         }
     }
 });
