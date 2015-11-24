227f4e3eed056040abed57495ffb4a942bf1be3b
John Hall <ojhsolutions@gmail.com>
2015-11-23 19:23:05 -0700

    Got the model for child directives bound to the parent directive

--- gov.doi.tracs.uploads.js
+++ gov.doi.tracs.uploads.js
@@ -1,29 +1,21 @@
 var tracsUploadsMod = angular.module('gov.doi.tracs.uploads', ['ui.bootstrap', 'gov.doi.tracs.forms', 'uploadModule']);
 
 tracsUploadsMod.
-    controller('fileUploadMetadataController', function($scope, $element, $attrs) {
-});
-
-tracsUploadsMod.
-directive('fileUploadWithMetadata', function() {
+directive('fileUploadWithMetadata', function () {
     return {
         restrict: 'E',
         scope: {
             tfId: '@',
             tfFileUrl: '@',
             tfFileSize: '@',
-            tfFileTypes: '@',
-            tfMetadata: '=',
-            tfModel: '=',
-            tfForm: '='
+            tfFileTypes: '@'
         },
         templateUrl: './templates/fileFormWithMetadata.html',
-        controllerAs: 'ctrl',
+        controllerAs: 'fUpMetaMain',
         controller: function ($scope, $element, $attrs) {
-            this.metadata = [];
+            this.metadata = {};
         },
         link: function(scope, element, attrs, ctrl) {
-            console.log("linkFn");
             //console.log(scope, element, attrs, ctrl);
         }
     }
